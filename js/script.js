094f6669989e3e3852d07d8bb75f5e5dcd01c200
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-31 17:51:07 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack
+// Created on: 
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radiusSphere = params.get('radiusSphere')
+  console.log(radiusSphere)
+
+  // process
+  const volumeSphere = (4 / 3) * Math.PI * Math.pow(radiusSphere, 3)
+  const dimensions = "<ul>\n<li>Radius of a Sphere = " + radiusSphere + "</li>"
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeSphere.toFixed(4) + ' cm³'
+}
